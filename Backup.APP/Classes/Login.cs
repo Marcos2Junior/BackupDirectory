dc0395299f7303110d41165b61cdc28a7814a79c
Marcos Junior <Mod.gluguer@gmail.com>
2020-12-10 23:47:15 -0300

    class login and startupApp

---
+++ Backup.APP/Classes/Login.cs
@@ -0,0 +1,35 @@
+﻿using Backup.APP.Library;
+using Backup.APP.Models;
+using System.IO;
+
+namespace Backup.APP.Classes
+{
+    public class Login
+    {
+        private readonly User User;
+
+        public Login(User user)
+        {
+            User = user;
+        }
+
+        public Response CheckLogin()
+        {
+            SettingsModel settingsModel = new SettingsModel
+            {
+                User = User
+            };
+
+            FoldersBackup foldersBackup = new FoldersBackup(settingsModel);
+            foldersBackup.ReadSettings();
+
+            if(foldersBackup.SettingsModel != null)
+            {
+                Properties.ActiveUser = foldersBackup.SettingsModel.User;
+                return new Response { Mensagem = "Login efetuado com sucesso!", Success = true };
+            }
+
+            return new Response { Mensagem = "Usuário ou senha incorreto", Success = false };
+        }
+    }
+}
---
+++ Backup.APP/Classes/StartupApp.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Backup.APP.Classes
+{
+    public class StartupApp
+    {
+        private readonly string registryKeyName = "BackupDirectory";
+        private readonly RegistryKey RegistryKey;
+        public StartupApp()
+        {
+            RegistryKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+        }
+        /// <summary>
+        /// Define app to start with windows
+        /// </summary>
+        public void SetStartup() => RegistryKey.SetValue(registryKeyName, Application.ExecutablePath);
+
+        /// <summary>
+        /// verify if app start with windows, return true if true
+        /// </summary>
+        public bool StartWithWindows => RegistryKey.GetValueNames().Any(x => x == registryKeyName);
+
+        /// <summary>
+        /// remove app of start with windows
+        /// </summary>
+        public void RemoveStartWithWindows() => RegistryKey.DeleteValue(registryKeyName);
+
+        /// <summary>
+        /// close the keys and flushes
+        /// </summary>
+        public void CloseRegistry() => RegistryKey.Close();
+    }
+}
